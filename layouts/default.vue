2cbbe98f213f2f059ce89450288f75d8db517b9c
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-01-22 10:55:21 +0800

    feat: refactor layout structure
    - Delete `app.vue` file
    - Add `layouts/default.vue` file
    - Add script, template, and style sections to `layouts/default.vue`
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Layout: default
+    <header>layout header</header>
+    <slot />
+    <footer>layout footer</footer>
+  </div>
+</template>
+
+<style scoped></style>
