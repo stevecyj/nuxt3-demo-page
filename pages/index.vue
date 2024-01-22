2e8e862fe160d836288d20a5a2ec40809f6902d7
stevecyj <ycsteven.tsao@gmail.com>
2024-01-22 23:36:59 +0800

    feat: add hello functionality to index page
    - Add a new file `pages/index.vue`
    - Add a line to import `useNuxtApp` from the `nuxt/composition-api` package in `pages/index.vue`
    - Add a line to use the `$hello` function from the `useNuxtApp` in `pages/index.vue`
    - Add a new file `plugins/hello.ts`
    - Add code to export a function that provides a `hello` function in `plugins/hello.ts`
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <script lang="ts" setup>
 // const { count, addCount } = useAddCount()
 
+const { $hello } = useNuxtApp()
+
 const store = useHomeStore()
 </script>
 
 <template>
   <h1>首頁: {{ store.count }}</h1>
+  <h2>{{ $hello("Dio") }}</h2>
   <button @click="store.addCount">add</button>
   <Home />
   <Box />
