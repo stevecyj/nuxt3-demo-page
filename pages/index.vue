147f42749f28ba4ab4476bab977f88c88882e998
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-01-24 11:51:45 +0800

    feat: add click event to "Btn" component
    - Add a click event to the "Btn" component
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@ const store = useHomeStore()
   <h1>首頁: {{ store.count }}</h1>
   <h2>{{ $hello("Dio") }}</h2>
   <button @click="store.addCount">add</button>
-  <Btn />
+  <Btn @click="store.addCount" />
   <Home />
   <Box />
 </template>
