923753213c50ad3ff5105d59c1a062098f843f22
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-02-16 15:26:25 +0800

    feat: add VDatePicker and VCalendar components to project
    - Add a `VDatePicker` component to the `pages/index.vue` file
    - Add a new file `plugins/calendar.ts` with the `VCalendar` plugin
    - Import and use the `VCalendar` plugin in the `plugins/calendar.ts` file
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,16 @@ import Btn from "@theme/Btn.vue"
 const { $hello } = useNuxtApp()
 
 const store = useHomeStore()
+
+const selectedDate = ref(new Date())
 </script>
 
 <template>
   <h1>首頁: {{ store.count }}</h1>
   <h2>{{ $hello("Dio") }}</h2>
   <h2 v-timeformat="1708066023624"></h2>
+  <VDatePicker v-model="selectedDate" />
+  <br />
   <button @click="store.addCount">add</button>
   <Btn @click="store.addCount" />
   <Home />
