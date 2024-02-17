ece718a9c208da3c90a7adce81769aace964126c
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-02-17 10:02:03 +0800

    feat: server middleware

--- pages/index.vue
+++ pages/index.vue
@@ -7,11 +7,23 @@ const { $hello } = useNuxtApp()
 const store = useHomeStore()
 
 const selectedDate = ref(new Date())
+
+const getApiData = async () => {
+  await fetch("/api/hello")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
 </script>
 
 <template>
   <nuxt-link to="/user">user</nuxt-link>
   <h1>首頁: {{ store.count }}</h1>
+  <button @click="getApiData">get api data</button>
   <h2>{{ $hello("Dio") }}</h2>
   <h2 v-timeformat="1708066023624"></h2>
   <VDatePicker v-model="selectedDate" />
