fd30c154c73623dd59f7164dc8a632589e503ba7
stevecyj <ycsteven.tsao@gmail.com>
2024-02-20 07:18:46 +0800

    feat: improve SEO meta tags for about page
    - Update SEO meta tags for the about page
    - Add new SEO meta tags for the about page
    - Remove unused code related to SEO meta tags
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- pages/about.vue
+++ pages/about.vue
@@ -1,16 +1,27 @@
 <script lang="ts" setup>
-useHead({
+// useHead({
+//   title: "關於我們 金龍呈祥 福滿金沙",
+//   meta: [
+//     { property: "og:title", content: "關於我們 金龍呈祥 福滿金沙" },
+//     { property: "og:url", content: "http://localhost:3000/about" },
+//     { property: "og:image", content: "http://localhost:3000/share.jpg" },
+//     { name: "description", content: "關於我們 金龍呈祥 福滿金沙" },
+//     {
+//       property: "og:description",
+//       content: "關於我們 金龍呈祥 福滿金沙",
+//     },
+//   ],
+// })
+
+useSeoMeta({
   title: "關於我們 金龍呈祥 福滿金沙",
-  meta: [
-    { property: "og:title", content: "關於我們 金龍呈祥 福滿金沙" },
-    { property: "og:url", content: "http://localhost:3000/about" },
-    { property: "og:image", content: "http://localhost:3000/share.jpg" },
-    { name: "description", content: "關於我們 金龍呈祥 福滿金沙" },
-    {
-      property: "og:description",
-      content: "關於我們 金龍呈祥 福滿金沙",
-    },
-  ],
+  description: "關於我們 金龍呈祥 福滿金沙",
+  ogDescription: "關於我們 金龍呈祥 福滿金沙",
+  ogTitle: "關於我們 金龍呈祥 福滿金沙",
+  ogImage: "",
+  twitterCard: "summary_large_image",
+  twitterSite: "",
+  twitterCreator: "",
 })
 </script>
 
