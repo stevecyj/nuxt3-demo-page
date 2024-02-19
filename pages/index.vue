641e622f23ce10124fe16f94c3d06e18e053967a
stevecyj <ycsteven.tsao@gmail.com>
2024-02-19 19:47:10 +0800

    chore: Install Tailwind CSS with Nuxt, standard

--- pages/index.vue
+++ pages/index.vue
@@ -32,6 +32,7 @@ const getApiData = async () => {
   <Btn @click="store.addCount" />
   <Home />
   <Box />
+  <h1 class="text-3xl text-blue-500 font-bold underline">Hello world!</h1>
 </template>
 
 <style scoped></style>
