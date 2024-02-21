0188bb6875c246123f80c007b971ef2a08bdca7d
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-02-21 17:07:26 +0800

    feat: refactor API calls to use `useAsyncData` pattern
    - Update the API calls to use `useAsyncData` instead of `useFetch`
    - Add a new API call for fetching cat images
    - Remove the old API calls for fetching dog and cat images
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- pages/many-fetch.vue
+++ pages/many-fetch.vue
@@ -1,7 +1,30 @@
 <script lang="ts" setup>
+import axios from "axios"
+
+// const [{ data: dog }, { data: cat }] = await Promise.all([
+//   useFetch("https://dog.ceo/api/breeds/image/random", { lazy: true }),
+//   useFetch("https://api.thecatapi.com/v1/images/search", { lazy: true }),
+// ])
+
+// axios 1個 api
+// const { data: dog } = await useAsyncData("axiosDog", async () => {
+//   const res = await axios.get("https://dog.ceo/api/breeds/image/random")
+//   console.log(res)
+//   return res.data
+// })
+
+// axios 2個 api
 const [{ data: dog }, { data: cat }] = await Promise.all([
-  useFetch("https://dog.ceo/api/breeds/image/random", { lazy: true }),
-  useFetch("https://api.thecatapi.com/v1/images/search", { lazy: true }),
+  useAsyncData("axiosDog", async () => {
+    const res = await axios.get("https://dog.ceo/api/breeds/image/random")
+    console.log(res)
+    return res.data
+  }),
+  useAsyncData("axiosCat", async () => {
+    const res = await axios.get("https://api.thecatapi.com/v1/images/search")
+    console.log(res)
+    return res.data
+  }),
 ])
 </script>
 
