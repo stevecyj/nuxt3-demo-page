2068ef92eeb0f1c41889f5c03695f95cc5aa917b
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-02-21 16:14:34 +0800

    feat: refactor data refreshing functionality
    - Add a `refresh` function to the `useAsyncData` hook
    - Update the `handleRefresh` function to call `refreshNuxtData`
    - Add a `Refresh` button to trigger the `handleRefresh` function
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- pages/async-data.vue
+++ pages/async-data.vue
@@ -1,14 +1,28 @@
 <script lang="ts" setup>
-const { data, pending, error } = await useAsyncData(
+const { data, pending, error, refresh } = await useAsyncData(
   "nuxt3test",
   () => $fetch("https://opentdb.com/api.php?amount=3"),
   { lazy: true }
 )
+
+// const handleRefresh = () => {
+//   refresh()
+// }
+
+const handleRefresh = () => {
+  refreshNuxtData("nuxt3test")
+}
 </script>
 
 <template>
   <div>
     <h1>useAsync</h1>
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      @click="handleRefresh"
+    >
+      Refresh
+    </button>
     <div class="w-6/12" v-if="pending">Loading ...</div>
     <div v-else-if="error" class="text-red-300">{{ error.message }}</div>
     <pre v-else>{{ data }}</pre>
