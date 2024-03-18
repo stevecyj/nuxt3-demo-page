63f1f1bbb7c41f0be3ab73a7309e50b1ac0a9d5f
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-03-18 14:46:32 +0800

    feat: gsap, scroll layer

--- pages/gsap/layers.vue
+++ pages/gsap/layers.vue
@@ -1,13 +1,81 @@
 <script lang="ts" setup>
 import transitionConfig from "@helpers/transitionConfig"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+
 definePageMeta({
   pageTransition: transitionConfig,
+})
+
+const { transitionState } = useTransition()
+const main = ref()
+let ctx: gsap.Context
+let scrollTween: gsap.core.Tween | null
+
+const goToSection = (i: number) => {
+  // Remove the GSAP instance with the specific ID
+  // to prevent memory leak
+  ctx.data.forEach((e: gsap.core.Tween) => {
+    if (e.vars && e.vars.id === "scrollTween") {
+      e.kill()
+    }
+  })
+  ctx.add(() => {
+    scrollTween = gsap.to(window, {
+      scrollTo: { y: i * window.innerHeight, autoKill: false },
+      duration: 1,
+      id: "scrollTween",
+      onComplete: () => (scrollTween = null),
+      overwrite: true,
+    })
+  })
+}
+
+watch(
+  () => transitionState.transitionComplete,
+  (newValue) => {
+    if (newValue) {
+      ctx = gsap.context((self) => {
+        const panels = self.selector?.(".panel")
+        panels.forEach((panel: Element, i: number) => {
+          ScrollTrigger.create({
+            trigger: panel,
+            start: "top bottom",
+            end: "+=200%",
+            onToggle: (self) => self.isActive && !scrollTween && goToSection(i),
+          })
+        })
+        ScrollTrigger.create({
+          start: 0,
+          end: "max",
+          snap: 1 / (panels.length - 1),
+        })
+      }, main.value) // <- Scope!
+    }
+  }
+)
+
+onUnmounted(() => {
+  ctx.revert() // <- Easy Cleanup!
 })
 </script>
 
 <template>
-  <div>
-    <h1>layers</h1>
+  <div ref="main">
+    <section class="description panel blue">
+      <div>
+        <h1>Layered pinning</h1>
+        <p>Use pinning to layer panels on top of each other as you scroll.</p>
+        <div class="scroll-down">
+          Scroll down
+          <div class="arrow"></div>
+        </div>
+      </div>
+    </section>
+    <section class="panel red">ONE</section>
+    <section class="panel orange">TWO</section>
+    <section class="panel purple">THREE</section>
+    <section class="panel green">FOUR</section>
   </div>
 </template>
 
